a1f363aa945bcc50cc6fc71124da6f4379610586
Pedro Fernandes <pedro92.gf@gmail.com>
2017-04-10 20:25:17 -0400

    Tab component

---
+++ Tab Component/Tab Component/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tab Component
+//
+//  Created by Pedro Guimarães fernandes on 2017-04-10.
+//  Copyright © 2017 Pedro Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Tab Component/Tab Component/ThirdViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Third View Loaded");
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
